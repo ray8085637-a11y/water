32a37e38fab07ac3284223e208e6db2089c61407
ray8085637-a11y <ray8085637@gmail.com>
2025-08-19 18:31:11 +0900

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://wmlstgssbtrftukgxupo.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtbHN0Z3NzYnRyZnR1a2d4dXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1OTIwODksImV4cCI6MjA3MTE2ODA4OX0.-t-fZ4QuaaV_BNSpA_ncqy50-SVADOdqLwWVZaH81II'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
